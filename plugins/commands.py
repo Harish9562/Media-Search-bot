05d19ac4d56a2195e95e1a6989a981072c4d34c2
Harish9562 <73013494+Harish9562@users.noreply.github.com>
2021-10-04 22:45:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,7 +100,7 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
+                        InlineKeyboardButton("DFBC Chat", url="https://t.me/DFBC_MCUc")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
